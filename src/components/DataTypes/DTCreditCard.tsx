ecb345abd550a00c4f6ce2b104d0a262d868ed6d
vikram1vicky@gmail.com <vikram1vicky@gmail.com>
2019-05-12 21:53:48 +0530

    Updated content for CC

--- src/components/DataTypes/DTCreditCard.tsx
+++ src/components/DataTypes/DTCreditCard.tsx
@@ -11,11 +11,7 @@ export default class DTCreditCard extends Component<IOnMount> {
     render() {
         return (<article className="creditCard">
             <h1>Credit Card Number</h1>
-            <p>This Data Type can be used to generate random Credit Card Number in&nbsp; 
-                 <span className="tag">English Color Name</span>&nbsp;,&nbsp;
-                 <span className="tag">Hexadecimal</span>&nbsp;,&nbsp; 
-                 <span className="tag">RGB Numeric</span>&nbsp;,&nbsp; 
-                <span className="tag">RGB Percentage</span>&nbsp; formats.</p>
+            <p>This Data Type can be used to generate random Credit Card Number in various formats.</p>
             <Availability inApp={true} inLib={true} />
             <h2>Options</h2>
             <p>It provides following options to configure:</p>
@@ -30,15 +26,40 @@ export default class DTCreditCard extends Component<IOnMount> {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>format</td>
-                        <td>EColorFormat</td>
-                        <td>EColorFormat.NAME</td>
-                        <td>Optional. 
-                            <pre>{` EColorFormat {
-    NAME = 0, // For English Color name
-    HEX_CODE = 1, // For Hexadecimal value
-    RGB_PERCENT = 2, // For RGB percentage value
-    RGB_NUMERIC = 3 // For RGB numeric value
+                        <td>separator</td>
+                        <td>String</td>
+                        <td>&lt;space&gt;</td>
+                        <td>Optional. separator is used to fill any character between every 4 digits.
+                            Provide empty string ('') to get value without any space or character.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>cardTypes</td>
+                        <td>ICCardType[]</td>
+                        <td><pre>{`[{
+    name: 'American Express',
+    formats: [{
+        iins: [34, 37],
+        range: {
+            min: 15,
+            max: 15
+        }
+    }]
+}]`}</pre></td>
+                        <td>Optional.
+                            <pre>{`ICCardType {
+    name?: string;
+    formats: ICCardFormat[];
+}
+
+ICCardFormat {
+    iins: Array<IMinMax | number>;
+    range: IMinMax;
+}
+
+IMinMax {
+    min: number;
+    max: number;
 }`}</pre></td>
                     </tr>
                 </tbody>
@@ -56,48 +77,80 @@ const creditCardJson1 = [{
 }];
 
 // Output for 1 record:
-[{ myCC: 'Red' }]
+[{ myCC: '370 0305 0952 7530' }]
 
 /** 
- * Sample 2: Get Hexadecimal Color value
+ * Sample 2: With Empty String as seperator
  */
 const creditCardJson2 = [{
     "name": "myCC",
     "type": "creditCard",
     "options": {
-        "format": 1
+        "separator": ""
     }
 }];
 
 // Output for 1 record:
-[{ myCC: '#B0C4DE' }]
+[{ myCC: '378554203655751' }]
 
 /** 
- * Sample 3: Get RGB Numeric Color value
+ * Sample 3: With "-" as separator and with iins as numeric array or range of numbers
  */
 const creditCardJson3 = [{
     "name": "myCC",
     "type": "creditCard",
     "options": {
-        "format": 3
+        "separator": "-",
+        "cardTypes": [{
+            "name": "Diners Club United States & Canada",
+            "formats": [{
+                "iins": [54, 55],
+                "range": { "min": 16, "max": 16 }
+            }]
+        }, {
+            "name": "JCB",
+            "formats": [{
+                "iins": [{
+                    "min": 3528, "max": 3589
+                }],
+                "range": { "min": 16, "max": 19 }
+            }]
+        }]
     }
 }];
 
 // Output for 1 record:
-[{ myCC: 'rgb(255, 109.65, 73.95)' }]`}
+[{ myCC: '3560-6609-1511-3386' }]`}
                 </code>
             </pre>
             <h3>Utility method:</h3>
             <pre>
                 <code className="language-javascript">
                     {`/** Default */
-creditCard(); // Outputs: 'Red'
+creditCard(); // Outputs: '370 0305 0952 7530'
 
-/** Get Hexadecimal Color value */
-creditCard({ format: 1 }); // Outputs: '#B0C4DE'
+/** With Empty String as seperator */
+creditCard({ separator: '' }); // Outputs: '378554203655751'
 
-/** Get RGB Numeric Color value */
-creditCard({ format: 3 }); // Outputs: 'rgb(255, 109.65, 73.95)'`}
+/** With "-" as separator and with iins as numeric array or range of numbers */
+creditCard({
+    separator: '-',
+    cardTypes: [{
+        name: 'Diners Club United States & Canada',
+        formats: [{
+            iins: [54, 55],
+            range: { min: 16, max: 16 }
+        }]
+    }, {
+        name: 'JCB',
+        formats: [{
+            iins: [{
+                min: 3528, max: 3589
+            }],
+            range: { min: 16, max: 19 }
+        }]
+    }]
+}); // Outputs: '3560-6609-1511-3386'`}
                 </code>
             </pre>
         </article>);
